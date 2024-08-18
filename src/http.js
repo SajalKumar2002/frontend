fb8b2cb2086b48206decaa547de0b15c43528825
Sajal Kumar <sajalkumar.gd4@gmail.com>
2024-08-18 17:18:04 +0530

    modified:   build/asset-manifest.json 	modified:   build/index.html 	renamed:    build/static/js/main.67bac095.js -> build/static/js/main.b762d2a8.js 	renamed:    build/static/js/main.67bac095.js.LICENSE.txt -> build/static/js/main.b762d2a8.js.LICENSE.txt 	renamed:    build/static/js/main.67bac095.js.map -> build/static/js/main.b762d2a8.js.map 	modified:   src/http.js

--- src/http.js
+++ src/http.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "15.206.74.44:5000",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
-    'Access-Control-Allow-Origin': 'http://localhost:5000',
+    'Access-Control-Allow-Origin': '15.206.74.44:5000',
   }, withCredentials: true,
 })
 
