8b57c1f9f76e207fb62c1c53dee081834cc555db
Sajal Kumar <sajalkumar.gd4@gmail.com>
2024-08-18 17:24:41 +0530

    modified:   src/http.js 	modified:   build/asset-manifest.json 	modified:   build/index.html 	deleted:    build/static/js/main.b762d2a8.js 	deleted:    build/static/js/main.b762d2a8.js.LICENSE.txt 	deleted:    build/static/js/main.b762d2a8.js.map 	build/static/js/main.ae310738.js 	build/static/js/main.ae310738.js.LICENSE.txt 	build/static/js/main.ae310738.js.map

--- src/http.js
+++ src/http.js
@@ -4,9 +4,9 @@ const http = axios.create({
   baseURL: "15.206.74.44:5000",
   headers: {
     Accept: "application/json",
-    "Content-Type": "application/json",
-    'Access-Control-Allow-Origin': '15.206.74.44:5000',
-  }, withCredentials: true,
+    "Content-Type": "application/json"
+  },
+  withCredentials: true,
 })
 
 export default http;
